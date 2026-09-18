f85077872e527fa1a51ac014a211af9b9ccee018
楠木 <157884200@qq.com>
2026-09-18 18:19:59 +0800

    fix: 修复显示弹窗问题

--- src/lib/components/ui/confirm-dialog/ConfirmDialog.svelte
+++ src/lib/components/ui/confirm-dialog/ConfirmDialog.svelte
@@ -34,7 +34,7 @@
 </script>
 
 <Dialog.Root bind:open>
-  <Dialog.Content class="max-w-md">
+  <Dialog.Content class="sm:max-w-md">
     <Dialog.Header>
       <Dialog.Title>{title}</Dialog.Title>
       {#if description}
